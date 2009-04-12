79706ad5a5cccfbdf5652570e25c810273d2a791
berta1108 <berta1108@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-04-12 21:07:16 +0000

    Se simplifico el manejo de excepciones. Se reemplazaron todas por una sola (para no volver complicados los bloques try catch, eran demasiadas excepciones de validacion).

--- Clases/app/Arquero.java
+++ Clases/app/Arquero.java
@@ -4,6 +4,12 @@ public class Arquero extends Jugador {
  
 	private int cantPenalesAtajados; 
 	private int cantGolesRecibidos;	 
+	
+	public Arquero(String nombre,int posicion,boolean isSuplente,String club){
+		
+		super(nombre,posicion,isSuplente,club);
+		
+	}
 	 
 	public void setAtajaPenal(int cantidad) {
             this.cantPenalesAtajados= cantidad;
