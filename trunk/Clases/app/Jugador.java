8fe585f41798a8155c4b05e0d3f9176f2c7a1316
matidigi99@gmail.com <matidigi99@gmail.com@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-04-13 04:28:52 +0000

    ahi hice los cambios bertiña Solo faltaron dos validaciones, talvez hay alguna boludez pero no veo mas estoy re ciego mañana seguimos

--- trunk/Clases/app/Jugador.java
+++ trunk/Clases/app/Jugador.java
@@ -12,11 +12,13 @@ public class Jugador {
 	private int precio;
 	private boolean juega;
 	private boolean isSuplente;
+	private int penalesAtajados;
 	private int cantPenalesErrados;
 	private boolean completoPartido;
 	private boolean isEstrella;
 	private String club;
-	
+	private boolean equipoGana;
+	private boolean jugoPartidoEntero;
 	public static final int ARQUERO = 1;
 	public static final int DEFENSOR = 2;
 	public static final int MEDIOCAMPISTA = 3;
@@ -29,16 +31,53 @@ public class Jugador {
 		this.isSuplente=isSuplente;
 		this.idJugador=CampeonatoSingleton.getInstancia().getIdJugadorNuevo();
 	}
-
+	public boolean getGana(){
+		return equipoGana;
+	}
+	private int getPuntaje(int goles,boolean equipoGana,int rojas,int tiempoRoja, int atajaPenal,int erraPenal,boolean jugoEntero){
+	int puntos=0;
+	int pos=getPosicion();
+	if(goles != 0){
+		switch (pos){        
+	    case ARQUERO: puntos=(puntos+4)*goles;
+	 	  break;
+	    case DELANTERO: puntos=(puntos+1)*goles;
+		  break;
+	    case DEFENSOR: puntos=(puntos+3)*goles;
+		  break;
+	    case MEDIOCAMPISTA: puntos=(puntos+2)*goles;
+	    	break;
+		}
+	}
+	if(getGana())
+		puntos= puntos+2;
+	else
+		puntos=puntos-1;
 	
+	if (rojas != 0){
+		if (tiempoRoja==1)
+			puntos=puntos-4;
+		else
+			puntos=puntos-2;	
+	}
+	//falta validar dos cosas la primera es que si es arquero y recibe un gol se le reste un punto.
+	//la segunda es qeu sea suplente o no juege esa fecha.
+	if (atajaPenal != 0) puntos=puntos+5;
+	if (erraPenal != 0 ) puntos=puntos-4;
+	if(!jugoEntero) puntos=puntos-1;
+	return puntos;
+	}
 	public void actualizarDatos(EstadisticasJugadorFecha estadisticas) {
 		goles=estadisticas.getGoles();
-		amarillas=estadisticas.getAmarillas()+ estadisticas.getTiempoAmarilla();
-		rojas=estadisticas.getRojas()+ estadisticas.getTiempoRoja();
+		equipoGana=estadisticas.isGanoFecha();
+		amarillas=estadisticas.getAmarillas();
+		penalesAtajados=estadisticas.getPenalesAtajados();
+		rojas=estadisticas.getRojas();
 		isEstrella=estadisticas.getEsEstrella();
+		jugoPartidoEntero=estadisticas.getCompletoPartido();
 		cantPenalesErrados=estadisticas.getPenalesErrados();
 		completoPartido= estadisticas.getCompletoPartido();
-		//aca calcula nuevo precio de jugador
+		precio=(COSTO_INICIAL_JUGADOR + 100 * getPuntaje(goles,equipoGana,rojas, estadisticas.getTiempoRoja(),penalesAtajados,cantPenalesErrados,jugoPartidoEntero));
 	}
 
 	public int getIdJugador() {
