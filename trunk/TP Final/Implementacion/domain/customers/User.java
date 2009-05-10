b676dd8445c6df0c1f58f10fe0d9c976d7cf45e5
matidigi99@gmail.com <matidigi99@gmail.com@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-05-10 21:32:22 +0000

    cambie algunas cositas si estan mal avisen

--- trunk/TP Final/Implementacion/domain/customers/User.java
+++ trunk/TP Final/Implementacion/domain/customers/User.java
@@ -3,20 +3,64 @@ package domain.customers;
 import java.util.Date;
 
 public class User {
+	private int Dni;
+	private String Name;
+	private String LastName;
+	private Date BirthDate;
+	private int Points;
 
 	public User createUser(int dni, String name, String lastName, Date birthDate) {
-		return null;
+		this.setDni(dni);
+		this.Name=name;
+		this.setLastName(lastName);
+		this.setBirthDate(birthDate);
+		return this;
 	}
 
 	public void addPoints(int points) {
-
+		this.setPoints(points);
 	}
 
 	public void acceptGroupInvitation(int idGrupo) {
 
 	}
+	public String getName(){
+		return Name;
+	}
 
 	public void declineGroupInvitation(int idGrupo) {
 
+	}
+
+	public void setBirthDate(Date birthDate) {
+		BirthDate = birthDate;
+	}
+
+	public Date getBirthDate() {
+		return BirthDate;
+	}
+
+	public void setPoints(int points) {
+		Points = points;
+	}
+
+	public int getPoints() {
+		return Points;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setDni(int dni) {
+		Dni = dni;
+	}
+
+	public int getDni() {
+		return Dni;
 	}
 }
