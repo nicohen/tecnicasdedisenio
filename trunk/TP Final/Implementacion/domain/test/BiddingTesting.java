b9d21b40f9669ba9a88b54dbc48ed341c6bb5146
matidigi99@gmail.com <matidigi99@gmail.com@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-05-13 19:57:02 +0000

    ahi hay dos test el primero es el mas basico que solamente oferta. El ultimo test valida la devolucion de los puntos a los usuarios que ofertaron pero sus ofertas fueron superadas.

--- trunk/TP Final/Implementacion/domain/test/BiddingTesting.java
+++ trunk/TP Final/Implementacion/domain/test/BiddingTesting.java
@@ -1,13 +1,13 @@
 package domain.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.Date;
 
-import org.junit.Test;
-
 import domain.auctions.Auction;
 import domain.auctions.AuctionType;
+import domain.auctions.Bid;
 import domain.auctions.IncrementalAuction;
 import domain.customers.User;
 import domain.products.Product;
@@ -16,7 +16,6 @@ import domain.utils.VariationRateFunction;
 
 public class BiddingTesting {
 
-	@Test
 	public void bidOnIncrementAuctionTest(){
 		Date dateOfBirth = new Date();
 		User aUser = new User(31733445, "Aníbal", "Lovaglio", dateOfBirth);
@@ -28,9 +27,31 @@ public class BiddingTesting {
 		assertTrue(anAuction.getAmountForNextBid()>1000);
 	}
 
-	@Test
 	public void takeNewBidTest() {
-		fail ("Not yet implemented");
+		Date dateOfBirth = new Date();
+		User aUser1 = new User(31733445, "Aníbal", "Lovaglio", dateOfBirth);
+		User aUser2 = new User(31733445, "German", "Mendez", dateOfBirth);
+		User aUser3 = new User(31733445, "Martin", "De luca", dateOfBirth);
+		aUser1.addPoints(1000);
+		aUser2.addPoints(1000);
+		aUser3.addPoints(1000);
+		
+		Product prize = null;
+		VariationRateFunction variationFunction=null;
+		Auction anAuction = new IncrementalAuction(prize, AuctionType.SINGLE, variationFunction, 500);
+		
+		Bid myBid1 = new Bid(aUser1, 200);
+		anAuction.takeNewBid(myBid1);
+		
+		Bid myBid2= new Bid(aUser2, 400);
+		anAuction.takeNewBid(myBid2);
+		
+		Bid myBid3 = new Bid(aUser3, 600);
+		anAuction.takeNewBid(myBid3);
+		assertTrue(aUser3.getPoints()== 400);
+		assertTrue(aUser1.getPoints()== 200);
+		
+		//fail ("Not yet implemented");
 	}
 	
 }
