acba11e0cc044c5fb0dfc2257ddbae3179ecdfe0
berta1108 <berta1108@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-05-20 14:39:09 +0000

    javadoc de querys

--- TP Final/Implementacion/domain/querys/Transaction.java
+++ TP Final/Implementacion/domain/querys/Transaction.java
@@ -2,10 +2,22 @@ package domain.querys;
 
 import java.util.Date;
 
+/**
+ * La clase Transaction es una generalizacion de todas las transacciones que maneja
+ * el sistema. 
+ * 
+ */
 public abstract class Transaction {
 	
 	protected Date occurrenceDate;
 	
+	/**
+	 * Inicializa la fecha en la cual se produjo la transaccion.
+	 *
+	 * @param Date
+	 *            fecha de transaccion
+	 *
+	 */
 	public Transaction (Date aDate){
 		this.occurrenceDate = aDate;
 	}
