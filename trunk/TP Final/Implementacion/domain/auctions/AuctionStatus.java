3401be6df92219d05fc6fc188318d148fa735913
alelovaglio <alelovaglio@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-05-20 07:16:57 +0000

    normalizo algunos comentarios del javadoc

--- trunk/TP Final/Implementacion/domain/auctions/AuctionStatus.java
+++ trunk/TP Final/Implementacion/domain/auctions/AuctionStatus.java
@@ -2,7 +2,7 @@ package domain.auctions;
 
 /**
  * Los distintos estados por los que pasa, o podría pasar, un remate desde que
- * se instancia hasta que se resuelva.
+ * se crea hasta que se resuelva.
  */
 public enum AuctionStatus {
 	NEW, ACTIVE, CLOSED, PENDING;
