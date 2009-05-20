9eff24957c4145bc23f6bc564fe40d62d5f429bf
alelovaglio <alelovaglio@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-05-20 07:24:57 +0000

    agrego algunos comentarios del javadoc

--- trunk/TP Final/Implementacion/domain/utils/VariationRateFunction.java
+++ trunk/TP Final/Implementacion/domain/utils/VariationRateFunction.java
@@ -1,10 +1,29 @@
 package domain.utils;
 
+/**
+ * Clase encargada de determinar pasos de variación en los valores de los
+ * remates.
+ */
 public class VariationRateFunction {
 
-	public VariationRateFunction(String functionExpression) {}
-	
-	public int nextDelta () {
-		return (int) Math.round(Math.random()*100);
+	/**
+	 * Define la expresión que se ha de utilizar para generar los valores de los
+	 * saltos
+	 * 
+	 * @param functionExpression
+	 *            expresión de la función matemática que se utilizará de
+	 *            referencia
+	 */
+	public VariationRateFunction(String functionExpression) {
+	}
+
+	/**
+	 * Devuelve el valor del siguiente incremento o decremento en el valor del
+	 * remate
+	 * 
+	 * @return el valor del siguiente salto
+	 */
+	public int nextDelta() {
+		return (int) Math.round(Math.random() * 100);
 	}
 }
