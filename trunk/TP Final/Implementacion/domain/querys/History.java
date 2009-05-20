c8f018ede92a83a181c9a35d8aab1e4376b98379
alelovaglio <alelovaglio@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-05-20 21:11:17 +0000

    removed warnings

--- trunk/TP Final/Implementacion/domain/querys/History.java
+++ trunk/TP Final/Implementacion/domain/querys/History.java
@@ -3,14 +3,12 @@ package domain.querys;
 import java.util.ArrayList;
 import java.util.List;
 
-import domain.auctions.AuctionType;
 import domain.auctions.Bid;
-import domain.auctions.Product;
 import domain.customers.Donation;
 import domain.customers.KeyExchange;
-import domain.utils.VariationRateFunction;
+
 /**
- * Clase singleton que representa al Historial del sistema, el cual registra 
+ * Clase singleton que representa al Historial del sistema, el cual registra
  * donaciones, canjeo de puntos por claves, ofertas y consultas
  * 
  * @see KeyExchange
@@ -24,24 +22,24 @@ public class History {
 	private List<Donation> donations;
 	private List<Bid> bids;
 	private List<Query> querys;
-	//singleton
+	// singleton
 	static private History history = new History();
-	
+
 	/**
-	 * Inicializa las estructuras necesarias para la implementación del Historial,
-	 * clase de instancia única utilizando el patrón Singleton.
+	 * Inicializa las estructuras necesarias para la implementación del
+	 * Historial, clase de instancia única utilizando el patrón Singleton.
 	 */
-	private History(){
+	private History() {
 		this.keyExchanges = new ArrayList<KeyExchange>();
 		this.donations = new ArrayList<Donation>();
 		this.bids = new ArrayList<Bid>();
 		this.querys = new ArrayList<Query>();
 	}
-	
-	public static History getInstance(){
+
+	public static History getInstance() {
 		return history;
 	}
-	
+
 	public List<KeyExchange> getKeyExchanges() {
 		return keyExchanges;
 	}
@@ -57,17 +55,20 @@ public class History {
 	public List<Query> getQuerys() {
 		return querys;
 	}
-	
-	public void addKeyExchange(KeyExchange keyExchange){
+
+	public void addKeyExchange(KeyExchange keyExchange) {
 		this.keyExchanges.add(keyExchange);
 	}
-	public void addDonation(Donation donation){
+
+	public void addDonation(Donation donation) {
 		this.donations.add(donation);
 	}
-	public void addBid(Bid bid){
+
+	public void addBid(Bid bid) {
 		this.bids.add(bid);
 	}
-	public void addQuery(Query query){
+
+	public void addQuery(Query query) {
 		this.querys.add(query);
 	}
 }
