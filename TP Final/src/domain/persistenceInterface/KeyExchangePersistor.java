e1e3b099f8270efc72e6fc0aa5e2a9bdd99340f3
agusbazzano <agusbazzano@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-11 01:20:27 +0000

    keyExchangePersistor (icompletisimo)

--- TP Final/src/domain/persistenceInterface/KeyExchangePersistor.java
+++ TP Final/src/domain/persistenceInterface/KeyExchangePersistor.java
@@ -3,6 +3,7 @@
  */
 package domain.persistenceInterface;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import domain.customers.Key;
@@ -14,9 +15,9 @@ import domain.customers.User;
  */
 public interface KeyExchangePersistor {
 
-	public KeyExchange[] getKeyExchangesForUser(User user);
-	public KeyExchange[] getKeyExchangesForUserSinceDate(User user, Date dateSince);
-	public KeyExchange[] getKeyExchangesForUserBetweenDates(User user, Date dateSince, Date dateTo);
+	public ArrayList<KeyExchange> getKeyExchangesForUser(User user);
+	public ArrayList<KeyExchange> getKeyExchangesForUserSinceDate(User user, Date dateSince);
+	public ArrayList<KeyExchange> getKeyExchangesForUserBetweenDates(User user, Date dateSince, Date dateTo);
 	
 	public KeyExchange getKeyExchangeForKey(String key);
 	public KeyExchange getKeyExchangeForKey(Key key);
