d68a62217f7f56e627690b35b7bf8a6554eb8c76
mgonzel <mgonzel@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-12 06:01:38 +0000

    todo el back end: arquitectura,  login, sesiones. ejemplo en test.web.admin

--- TP Final/src/api/web/mvc/controller/BackEndControllerServlet.java
+++ TP Final/src/api/web/mvc/controller/BackEndControllerServlet.java
@@ -2,18 +2,21 @@ package api.web.mvc.controller;
 
 import java.util.HashMap;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import api.web.login.admin.views.AdminExceptionView;
+import api.web.mvc.view.View;
 import api.web.session.SessionValidation;
 import api.web.session.exception.InvalidSessionAccessException;
 import api.web.session.exception.NotLoggedInException;
 
+
 /**
  * Controller abstracto para las paginas del estilo back-end
- * 
  * @author mgonzalez
- * 
+ *
  */
 public abstract class BackEndControllerServlet extends
 		AbstractControllerServlet {
@@ -22,42 +25,63 @@ public abstract class BackEndControllerServlet extends
 
 	@Override
 	protected void execute(HttpServletRequest req, HttpServletResponse res) {
-		// Atributos del request
-		HashMap<String, Object> requestAttributes = new HashMap<String, Object>();
-
-		HashMap<String, Object> parameters = loadRequestParameters(req);
+		//Atributos del request
+		HashMap<String,Object> requestAttributes = new HashMap<String, Object>();
+		
+		HashMap<String,Object> parameters = loadRequestParameters(req);
 		requestAttributes.put("parameters", parameters);
-
-		// Contexto
-
-		// carga de Cookies
-		HashMap<String, String> cookies = loadCookies(req);
+		
+		//Contexto
+		
+		//carga de Cookies
+		HashMap<String, String> cookies = loadCookies(req); 
 		requestAttributes.put("cookies", cookies);
-
-		// validacion de sesion
+		
+		
+		//validacion de sesion
 		try {
 			validateAdminSession(cookies);
-		} catch (NotLoggedInException nlie) {
-			// showLoginView(req, res);
-		} catch (InvalidSessionAccessException isae) {
-			// showInvalidAccessView(req, res)
-		} catch (Exception e) {
-			// showGeneralAdminLoginErrorView(req, res);
-		}
+			
+			executeView(req,res,requestAttributes,getServletContext(), parameters);
+			return;
+		} catch (Exception e){
 
-		// validacion de sesion
-		// permiso de administracion
+			try {//Si llego aca es porque tengo que mostrar una vista de exception.
+				View view = new AdminExceptionView(req,res,requestAttributes,getServletContext(),parameters);
+				((AdminExceptionView) view).setAdminException(e);	
+				view.execute();
+			} catch (Exception finalE){
+				System.out.println("Error, no pudo mostrarse la view de AdminException");
+			}
+		}
+		
+
 
 	}
 
-	private void validateAdminSession(HashMap<String, String> cookies)
-			throws Exception {
-		String[] login = ((String) cookies.get("adminuser")).split("-");
+
+	private void validateAdminSession(HashMap<String, String> cookies) 
+			throws Exception{
+		
+		String adminCookie = (String)cookies.get("adminUser");
+		if (adminCookie == null){
+			throw new NotLoggedInException("Not logged in.");
+		}
+		String [] login = adminCookie.split("-");
+		
+		if (login.length<2){
+			throw new NotLoggedInException("Not logged in.");
+		}
 		int userId = Integer.parseInt(login[0]);
 		long hash = Long.parseLong(login[1]);
 
-		SessionValidation.validateAdminSession(userId, hash);
-
+		if (!SessionValidation.validateAdminSession(userId, hash)){
+			throw new InvalidSessionAccessException("Invalid user/password for user: "+userId);
+		}
+		
 	}
+
+
+
 
 }
