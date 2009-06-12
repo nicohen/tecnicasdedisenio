507114b98e55e488fceb05bf6dfe356779334579
mgonzel <mgonzel@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-12 06:02:17 +0000

    todo el back end: arquitectura,  login, sesiones. ejemplo en test.web.admin

---
+++ trunk/TP Final/src/api/web/login/admin/views/AdminExceptionView.java
@@ -0,0 +1,59 @@
+package api.web.login.admin.views;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import api.web.mvc.view.BackEndHtmlView;
+import api.web.text.LibTxt;
+
+public class AdminExceptionView extends BackEndHtmlView {
+
+	public AdminExceptionView(HttpServletRequest req, HttpServletResponse res,
+			HashMap<String, Object> requestAttributes,
+			ServletContext servletContext,
+			HashMap<String, Object> requestParameters) throws Exception {
+		super(req, res, requestAttributes, servletContext, requestParameters);
+	}
+
+	private Exception adminException;
+	
+	public Exception getAdminException() {
+		return adminException;
+	}
+
+	public void setAdminException(Exception adminException) {
+		this.adminException = adminException;
+	}
+
+	@Override
+	protected void doHtmlBody() {
+
+		out.println(adminException.getMessage());
+		out.println("<!-- STACK: \n");
+		out.println(formatException());
+		out.println("-->");
+	}
+
+	private String formatException() {
+			if (adminException != null) {
+				
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw, true);
+	
+
+				adminException.printStackTrace(pw);
+				pw.flush();
+				sw.flush();
+	
+				return sw.toString();
+			} else {
+				return "";
+			}
+	}
+
+}
