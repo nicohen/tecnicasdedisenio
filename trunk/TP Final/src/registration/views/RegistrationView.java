936d862ff08c6d987134b0f46a168c10c17eed50
berta1108 <berta1108@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-14 02:21:36 +0000

    Agregada version preliminar de registration. Arregladas cosas del ofertar (faltaba catchear exception de Credito Insuficiente)

---
+++ trunk/TP Final/src/registration/views/RegistrationView.java
@@ -0,0 +1,38 @@
+package registration.views;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import api.web.cache.HtmlCache;
+import api.web.mvc.view.HtmlView;
+import api.web.text.LibTxt;
+
+
+public class RegistrationView extends HtmlView {
+
+	public RegistrationView(HttpServletRequest req, HttpServletResponse res,
+			HashMap<String, Object> requestAttributes, ServletContext servletContext, HashMap<String,Object> requestParameters) throws Exception {
+		super(req, res, requestAttributes, servletContext,requestParameters);
+		
+	}
+
+	@Override
+	protected void doHtmlBody() {
+		String html = HtmlCache.getHtml(relativePath,"registration/RegistrationView");
+		String urlRedir = (String)requestParameters.get("urlredir"); 
+		if (urlRedir == null || urlRedir.length()==0){
+			urlRedir = req.getRequestURI();
+
+			String queryString = req.getQueryString();
+			if (queryString != null && queryString.length()>0){
+				urlRedir+="?"+ queryString;
+			}
+		}
+		
+		out.println(html);
+	}
+
+}
