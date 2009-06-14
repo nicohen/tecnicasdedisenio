49b391f9b318ec93b242ae5a4c7182a9446632eb
berta1108 <berta1108@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-14 21:54:27 +0000

    Servlet para que el usuario vea sus remates

--- trunk/TP Final/src/customers/auctions/views/UserAuctionsView.java
+++ trunk/TP Final/src/customers/auctions/views/UserAuctionsView.java
@@ -1,15 +1,23 @@
 package customers.auctions.views;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import persistence.AuctionPersistorImplementation;
+import persistence.BidderPersistor;
 
 import api.web.cache.HtmlCache;
 import api.web.mvc.view.HtmlView;
 import api.web.text.LibTxt;
 import domain.auctions.Auction;
+import domain.auctions.IncrementalAuction;
+import domain.auctions.ReverseAuction;
+import domain.customers.Bidder;
 
 public class UserAuctionsView extends HtmlView {
 
@@ -23,16 +31,32 @@ public class UserAuctionsView extends HtmlView {
 
 	@Override
 	protected void doHtmlBody() throws Exception {
-		String auctionsListView = HtmlCache.getHtml(relativePath, "auction/UserAuctionsListView");
+		String auctionsListView = HtmlCache.getHtml(relativePath, "customers/UserAuctionsListView");
+		
+		String userName = null;
+		Cookie[] c = req.getCookies();
+		for (int i = 0; i < c.length; i++) {
+			if ("userName".equals(c[i].getName())) {
+				userName = c[i].getValue();
+			}
+		}
+		Bidder bidder= BidderPersistor.getBidderPersistorInstance().getUser(userName);
+		ArrayList<IncrementalAuction> auctions = AuctionPersistorImplementation.getInstance().getIncrementalAuctionsForBidder(bidder);
 
+		StringBuilder strB = new StringBuilder();
 		
-		
-		//out.println(LibTxt.replace(auctionsListView,"##AUCTIONS##",strB.toString()));
+		if(auctions!=null) {
+			for(IncrementalAuction auction : auctions) {
+				strB.append(getAuctionInfo(auction));
+			}
+		}
+
+		out.println(LibTxt.replace(auctionsListView,"##AUCTIONS##",strB.toString()));
 
 	}
 
 	private String getAuctionInfo(Auction auction) {
-		String auctionListView = HtmlCache.getHtml(relativePath, "auction/AuctionListView");
+		String auctionListView = HtmlCache.getHtml(relativePath, "customers/UserAuctionListView");
 		auctionListView = LibTxt.replace(auctionListView,"##URL##",String.valueOf(auction.getAuctionId()));
 		auctionListView = LibTxt.replace(auctionListView,"##TITLE##",auction.getPrize().getDescription());
 		auctionListView = LibTxt.replace(auctionListView,"##POINTS##",String.valueOf(auction.getValue()));
