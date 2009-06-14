32616dc7cccbfacd113f4eeb1af3262fcdf8bbb6
nicohen <nicohen@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-14 22:53:11 +0000

    Modulo de donaciones

---
+++ trunk/TP Final/src/customers/donations/DonationController.java
@@ -0,0 +1,60 @@
+package customers.donations;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import persistence.BidderPersistor;
+
+import api.web.mvc.controller.FrontEndControllerServlet;
+import api.web.mvc.view.View;
+import api.web.text.LibWeb;
+import customers.donations.views.DonationView;
+import domain.customers.Donation;
+import domain.customers.User;
+import domain.exceptions.DonationAlreadyInstanciatedException;
+
+public class DonationController extends FrontEndControllerServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void executeView(HttpServletRequest req, HttpServletResponse res,
+			HashMap<String, Object> requestAttributes,
+			ServletContext servletContext,
+			HashMap<String, Object> requestParameters) throws Exception {
+
+		String pointsStr = LibWeb.getParameter(req, "points");
+		Integer points = null;
+		try {
+			points = Integer.parseInt(pointsStr);
+			String userName = null;
+			Cookie[] c = req.getCookies();
+			for(int i=0;i<c.length;i++) {
+				if("userName".equals(c[i].getName())) {
+					userName = c[i].getValue();
+				}
+			}
+			User aUser = BidderPersistor.getBidderPersistorInstance().getUser(userName);
+
+			try {
+				Donation.buildExistantDonation(aUser, aUser.getGroupOfUser(), points, new Date(System.currentTimeMillis()));
+			} catch(DonationAlreadyInstanciatedException e) {
+				throw new RuntimeException("La donacion ya fue instanciada anteriormente",e);
+			}
+		} catch(NumberFormatException e) {
+			View view = new DonationView(req, res, requestParameters, servletContext, requestParameters);
+			view.execute();
+		}
+		
+	}
+
+	@Override
+	protected boolean getValidated(HttpServletRequest req) {
+		return true;
+	}
+}
---
+++ trunk/TP Final/src/customers/donations/views/DonationView.java
@@ -0,0 +1,27 @@
+package customers.donations.views;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import api.web.cache.HtmlCache;
+import api.web.mvc.view.HtmlView;
+
+public class DonationView extends HtmlView {
+
+	public DonationView(HttpServletRequest req, HttpServletResponse res,
+			HashMap<String, Object> requestAttributes,
+			ServletContext servletContext,
+			HashMap<String, Object> requestParameters) throws Exception {
+		super(req, res, requestAttributes, servletContext, requestParameters);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doHtmlBody() throws Exception {
+		out.println(HtmlCache.getHtml(relativePath,"customers/donations/donation"));
+	}
+
+}
