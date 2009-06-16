e2cc1c47320a6ddcbfad143497daa484cfe18432
nicohen <nicohen@1cfbbbbe-26bc-11de-a2b9-47ed1c40409f>
2009-06-16 00:43:10 +0000

    Finalizado el modulo de intercambio de claves

--- trunk/TP Final/src/customers/keyexchange/views/KeyExchangeView.java
+++ trunk/TP Final/src/customers/keyexchange/views/KeyExchangeView.java
@@ -8,12 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import persistence.BidderPersistor;
-import persistence.KeyExchangesPersistor;
+import persistence.KeysPersistor;
 import api.web.cache.HtmlCache;
 import api.web.mvc.view.HtmlView;
-import api.web.text.LibTxt;
-import domain.customers.KeyExchange;
+import api.web.text.LibWeb;
+import domain.customers.Key;
 import domain.customers.User;
+import domain.exceptions.AlreadyUsedKeyException;
 
 public class KeyExchangeView extends HtmlView {
 
@@ -30,33 +31,33 @@ public class KeyExchangeView extends HtmlView {
 		String html = HtmlCache.getHtml(relativePath,
 				"customers/keyexchange/KeyExchange");
 
-		String userName = null;
-		Cookie[] c = req.getCookies();
-		for (int i = 0; i < c.length; i++) {
-			if ("userName".equals(c[i].getName())) {
-				userName = c[i].getValue();
+		if(!"".equals(LibWeb.getParameter(req, "points"))) {
+			String userName = null;
+			Cookie[] c = req.getCookies();
+			for (int i = 0; i < c.length; i++) {
+				if ("userName".equals(c[i].getName())) {
+					userName = c[i].getValue();
+				}
+			}
+	
+			String key = LibWeb.getParameter(req, "points");
+			KeysPersistor keyExchangesPersistor = KeysPersistor.getKeysPersistorInstance();
+
+			try {
+				Key keyExchange = keyExchangesPersistor.getKeyForAlphanumeric(key);
+	
+				if (keyExchange == null)
+					out.println("Error, no se encuentra la clave ingresada en el sistema");
+				else {
+					User exchangingUser = BidderPersistor.getBidderPersistorInstance().getUser(userName);
+					exchangingUser.addPoints(keyExchange.getPointsToExchange());
+					BidderPersistor.getBidderPersistorInstance().saveUser(exchangingUser);
+					out.println("Carga de puntos exitosa! Saldo actual:" + exchangingUser.getPoints());
+				}
+			} catch (AlreadyUsedKeyException e) {
+				out.println("Error, la clave ["+key+"] ya fue utilizada anteriormente");
 			}
 		}
-
-		String key = (String) requestParameters.get("key");
-		KeyExchangesPersistor keyExchangesPersistor = KeyExchangesPersistor
-				.getKeyExchangesPersistorInstance();
-		try {
-			KeyExchange keyExchange = keyExchangesPersistor
-					.getKeyExchangeForKey(key);
-
-			if (keyExchange == null)
-				html = LibTxt.replace(html, "##RESULT##","Error, no se encuentra la clave ingresada en el sistema");
-			else {
-				User exchangingUser = BidderPersistor.getBidderPersistorInstance().getUser(userName);
-				exchangingUser.addPoints(keyExchange.getPoints());
-				BidderPersistor.getBidderPersistorInstance().saveUser(exchangingUser);
-				html = LibTxt.replace(html, "##RESULT##","Carga de puntos exitosa! Saldo actual:" + exchangingUser.getPoints() );
-			}
-		} catch (Exception e) {
-			html = LibTxt.replace(html, "##RESULT##","Error en carga de puntos");
-		}
-
 		out.println(html);
 	}
 
